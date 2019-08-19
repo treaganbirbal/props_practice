330ccbed084038a3b3f143fe70bf0f7eff37d107
Treagan Birbal <treaganbirbal@pursuit.org>
2019-08-19 13:48:52 -0400

    created state for allPosts in app.js, passed it to Feed component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import blog_posts from './blog_posts.json';
+import Feed from './feed.js';
+import Post from './post.js';
 
-function App() {
+class App extends React.Component {
+  state = {
+    allPosts: blog_posts,
+  }
+  render(){
+    const {allPosts} = this.state
   return (
     <div className="App">
-      <>
-        Test
-      </>
+      
+        <Post allPosts={ allPosts } />
+      
     </div>
   );
+  }
 }
 
 export default App;
---
+++ src/feed.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Feed = (props) =>{
+            return(
+                <>FEED</>
+            )
+}
+
+export default Feed;
